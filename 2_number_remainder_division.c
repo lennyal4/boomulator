79bc12bcc662c0a5dd56f53fbb7e0fba406549d5
lennyal4 <53537346+lennyal4@users.noreply.github.com>
2020-05-29 10:57:20 -0400

    Update and rename division.c to 2_number_remainder_division.c

--- 2_number_remainder_division.c
+++ 2_number_remainder_division.c
@@ -2,12 +2,18 @@
 
 int main(void){
   int t,y;
+  //take both of my numbers
   printf("enter the divisor(or first number in your division problem):");
   scanf("%d",&y);
+  //take divisor
   printf("enter the dividend(or the second number in your equation):");
   scanf("%d",&t);
+  //take dividend
   int x= y%t;
+  // this is not the division problem but rather the modulo (or the remainder)
   int x_but_actually_the_number=y/t;
+  //take the divison and convert it to a integer (removing the decimals)
   printf("%d divided by %d equals %d with a remainder of %d\n",y,t,x_but_actually_the_number,x);
+  //display
   return 0;
 }
